41f0eac5cdff509dd0fe9b8e8f572249f2d489a5
53VICARIUS <deuphorique@gmail.com>
2022-12-08 09:27:22 +0100

    Affinement de l'IU

--- fonctions.py
+++ fonctions.py
@@ -1,57 +1,61 @@
 import fltk
+from variables import *
 
 
-def plateau_de_jeu_9(version, taille_fenetre):
-    '''
-    Pour taille_fenetre = 600 :
-    taille_fenetre // 2 = 300
-    taille_fenetre // 3 = 200
-    (taille_fenetre // 3) * 2 = 400
-    taille_fenetre // 6 = 100 (<- à changer car '6' dépend de '600' et non de
-    taille_fenetre).
-    '''
+def plateau_de_jeu_9(version, taille_plateau):
 
     liste_points_possibles = []
-    rayon = 5
+    rayon_intersection = 5
     epaisseur_trait = 3
 
     # Création des lignes
-    fltk.ligne(taille_fenetre // 2, 0, taille_fenetre // 2,
-               taille_fenetre // 3,
+    fltk.ligne(FENETRE_X // 3,
+               FENETRE_Y // 5 + taille_plateau // 2,
+               FENETRE_X // 3 + taille_plateau // 3,
+               FENETRE_Y // 5 + taille_plateau // 2,
                epaisseur=epaisseur_trait,
                couleur="black")
-    fltk.ligne(0, taille_fenetre // 2, taille_fenetre // 3,
-               taille_fenetre // 2,
+    fltk.ligne(FENETRE_X // 3 + taille_plateau // 2,
+               FENETRE_Y // 5,
+               FENETRE_X // 3 + taille_plateau // 2,
+               FENETRE_Y // 5 + taille_plateau // 3,
                epaisseur=epaisseur_trait,
                couleur="black")
-    fltk.ligne(taille_fenetre, taille_fenetre // 2, (taille_fenetre // 3) * 2,
-               taille_fenetre // 2,
+    fltk.ligne(FENETRE_X // 3 + (taille_plateau // 3) * 2,
+               FENETRE_Y // 5 + taille_plateau // 2,
+               FENETRE_X // 3 + taille_plateau,
+               FENETRE_Y // 5 + taille_plateau // 2,
                epaisseur=epaisseur_trait,
                couleur="black")
-    fltk.ligne(taille_fenetre // 2, taille_fenetre, taille_fenetre // 2,
-               (taille_fenetre // 3) * 2,
+    fltk.ligne(FENETRE_X // 3 + taille_plateau // 2,
+               FENETRE_Y // 5 + taille_plateau,
+               FENETRE_X // 3 + taille_plateau // 2,
+               FENETRE_Y // 5 + (taille_plateau // 3) * 2,
                epaisseur=epaisseur_trait,
                couleur="black")
 
-    # 200, 100 etc sont des valeurs obtenues en divisant taille_fenetre par
-    # 3 ou 6 (car taille_fenetre == 600) et doivent être remplacée par des
+
+    # 200, 100 etc sont des valeurs obtenues en divisant taille_plateau par
+    # 3 ou 6 (car taille_plateau == 600) et doivent être remplacée par des
     # variables
-    for p in range(0, taille_fenetre // 3 + 1, taille_fenetre // 6):
+    for p in range(0, taille_plateau // 3 + 1, taille_plateau // 6):
 
         # Création des carrés : 'range(3)' pour trois carrés
         for carre in range(3):
-            fltk.rectangle(0 + p, 0 + p, taille_fenetre - p,
-                           taille_fenetre - p,
+            fltk.rectangle(FENETRE_X // 3 + p,
+                           FENETRE_Y // 5 + p,
+                           FENETRE_X // 3 + taille_plateau - p,
+                           FENETRE_Y // 5 + taille_plateau - p,
                            epaisseur=3)
 
-        for x in range(0 + p, taille_fenetre - p + 1,
-                       taille_fenetre // 2 - p):
-            for y in range(0 + p, taille_fenetre - p + 100,
-                           taille_fenetre // 2 - p):
+        for x in range(FENETRE_X // 3 + p, FENETRE_X // 3 + taille_plateau - p + 1,
+                       taille_plateau // 2 - p):
+            for y in range(FENETRE_Y // 5 + p, FENETRE_Y // 5 + taille_plateau - p + 100,
+                           taille_plateau // 2 - p):
 
-                if not (x == taille_fenetre // 2
-                        and y == taille_fenetre // 2):
-                    fltk.cercle(x, y, rayon,
+                if not (x == FENETRE_X // 3 + taille_plateau // 2
+                        and y == FENETRE_Y // 5 + taille_plateau // 2):
+                    fltk.cercle(x, y, rayon_intersection,
                                 couleur='black',
                                 remplissage='black')
 
@@ -61,19 +65,26 @@ def plateau_de_jeu_9(version, taille_fenetre):
     return liste_points_possibles
 
 
-def intersection_survolee(liste_points_possibles):
+def interface():
+    dd = 4
 
-    rayon = 5
+
+def tour_joueur():
+    dd = 2
+
+
+
+def intersection_survolee(liste_points_possibles):
 
     for i in range(len(liste_points_possibles)):
         x_point, y_point = liste_points_possibles[i]
 
-        if fltk.abscisse_souris() > x_point - rayon \
-        and fltk.ordonnee_souris() > y_point - rayon \
-        and fltk.abscisse_souris() < x_point + rayon \
-        and fltk.ordonnee_souris() < y_point + rayon:
+        if fltk.abscisse_souris() > x_point - rayon_intersection \
+        and fltk.ordonnee_souris() > y_point - rayon_intersection \
+        and fltk.abscisse_souris() < x_point + rayon_intersection \
+        and fltk.ordonnee_souris() < y_point + rayon_intersection:
 
-            fltk.cercle(x_point, y_point, rayon,
+            fltk.cercle(x_point, y_point, rayon_intersection,
                         couleur='blue',
                         remplissage='blue',
                         tag='point_survolé')
@@ -83,25 +94,26 @@ def efface_intersection_survolee():
 
 
 def intersection(liste_points_possibles, ev):
-    rayon = 5
+
 
     for i in range(len(liste_points_possibles)):
         x_point, y_point = liste_points_possibles[i]
 
-        if fltk.abscisse(ev) > x_point - rayon \
-        and fltk.ordonnee(ev) > y_point - rayon \
-        and fltk.abscisse(ev) < x_point + rayon \
-        and fltk.ordonnee(ev) < y_point + rayon:
+        if fltk.abscisse(ev) > x_point - rayon_intersection \
+        and fltk.ordonnee(ev) > y_point - rayon_intersection \
+        and fltk.abscisse(ev) < x_point + rayon_intersection \
+        and fltk.ordonnee(ev) < y_point + rayon_intersection:
+
             print('intersection')
 
 
 def affichage():
-    taille_fenetre = 600
-    fltk.cree_fenetre(taille_fenetre, taille_fenetre)
-    # Pour le fond
-    fltk.rectangle(-10, -10, 1000, 1000, remplissage='#bababa')
 
-    liste_points_possibles = plateau_de_jeu_9('A', taille_fenetre)
+    fltk.cree_fenetre(FENETRE_X, FENETRE_Y)
+    # Pour le fond
+    fltk.rectangle(-10, -10, 9000, 9000, remplissage='#bababa')
+
+    liste_points_possibles = plateau_de_jeu_9('A', taille_plateau)
 
     continuer = True
     while continuer:
@@ -119,5 +131,4 @@ def affichage():
             print(fltk.abscisse(ev), fltk.ordonnee(ev))
 
         fltk.mise_a_jour()
-
     fltk.ferme_fenetre()
---
+++ variables.py
@@ -0,0 +1,5 @@
+# Variables affichage
+FENETRE_X = 900
+FENETRE_Y = 600
+taille_plateau = 300
+rayon_intersection = 5
